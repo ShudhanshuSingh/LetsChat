3ff71dcc00778c5eb01111b4394cc5aeb2a84d45
ShudhanshuSingh <shudhanshusingh051@gmail.com>
2020-08-10 12:33:23 +0530

    New update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import React from "react";
 import "./App.css";
-import { Button, TextField, FormControl } from "@material-ui/core";
+import {
+  Button,
+  TextField,
+  FormControl,
+  IconButton,
+  AppBar,
+  Toolbar,
+  Typography,
+} from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import Messages from "./Messages";
 import db from "./firebase";
@@ -9,9 +17,18 @@ import Picker, { SKIN_TONE_MEDIUM_DARK } from "emoji-picker-react";
 import "./emoji.css";
 import FlipMove from "react-flip-move";
 import SelectInput from "@material-ui/core/Select/SelectInput";
+import SendIcon from "@material-ui/icons/Send";
+import MicIcon from "@material-ui/icons/Mic";
+// import ReactAutoScroll from 'react-to-target-auto-scroll';
+// import { animateScroll } from "react-scroll";
+import ScrollToBottom from "react-scroll-to-bottom";
+import SentimentSatisfiedTwoToneIcon from "@material-ui/icons/SentimentSatisfiedTwoTone";
+
 function App() {
   const [input, setInput] = React.useState("");
-  const [messages, setMessages] = React.useState([{id:2,message:{message:"Hello", username:"SHudh"}}]);
+  const [messages, setMessages] = React.useState([
+    { id: 2, message: { message: "Happy Chatting!!", username: "LetsChat" } },
+  ]);
   const [username, setUsername] = React.useState("");
   const [chosenEmoji, setChosenEmoji] = React.useState(null);
   const sendMessage = (event) => {
@@ -27,15 +44,17 @@ function App() {
 
   React.useEffect(() => {
     db.collection("messages")
-      .orderBy("timestamp", "desc")
+      .orderBy("timestamp", "asc")
       .onSnapshot((snapshot) => {
-        setMessages(snapshot.docs.map(doc => ({id:doc.id, message:doc.data()})));
+        setMessages(
+          snapshot.docs.map((doc) => ({ id: doc.id, message: doc.data() }))
+        );
       });
   }, []);
 
   const nameList = [
     "Willard",
-    "Sadie", 
+    "Sadie",
     "Ashley",
     "Nelson",
     "Hadden",
@@ -45,58 +64,87 @@ function App() {
     "Balasubramanian",
   ];
   React.useEffect(() => {
-    var name = prompt("Hey! Please enter your username...else we will give you a name of our choice");
+    var name = prompt(
+      "Hey! Please enter your username...else we will give you a name of our choice"
+    );
     if (name === "") {
       setUsername(nameList[Math.floor(Math.random() * nameList.length)]);
     } else {
       setUsername(name);
     }
     console.log(name);
-
-    // setUsername(prompt("Hey! Please enter your username..."));
   }, []);
+  const emojiContainer = () => {
+    var x = document.querySelector(".emojiContainer");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  };
 
   const onEmojiClick = (event, emojiObject) => {
     setChosenEmoji(emojiObject);
-    // setInput(input + emojiObject.emoji);
-    console.log(emojiObject.emoji);
-    setInput(input + emojiObject.emoji);
   };
+
+  React.useEffect(() => {
+    if (chosenEmoji != null) {
+      setInput(input + chosenEmoji.emoji);
+    }
+  }, [chosenEmoji]);
+
+  // Mic part
 
   return (
     <div className="App">
-      <h2>LetsChat!! 🐱‍🏍</h2>
-      <h2>Welcome {username}🐱‍🏍</h2>
-      <FormControl>
-        <TextField
-          id="standard-basic"
-          label="Type..."
-          value={input}
-          onChange={(event) => setInput(event.target.value)}
-        />
-        <Button
-          type="submit"
-          variant="contained"
-          color="secondary"
-          disabled={!input}
-          onClick={sendMessage}
-        >
-          send
-        </Button>
+      <AppBar position="static" className="appBar">
+        <Toolbar className="appToolBar">
+          <Typography variant="h6">LetsChat</Typography>
+          <Typography variant="h6">Welcome {username}</Typography>
+          <Typography variant="h6">Made with 💖 by Shudhanshu</Typography>
+        </Toolbar>
+      </AppBar>
+
+      <form className="app__form">
+        <FormControl className="app__FormControl">
+          <TextField
+            className="app__input"
+            id="standard-basic"
+            label="Type..."
+            value={input}
+            // onChange={(event) => setInput(event.target.value + chosenEmoji)}
+            onChange={(event) => setInput(event.target.value)}
+          />
+          <IconButton color="secondary" onClick={emojiContainer}>
+            <SentimentSatisfiedTwoToneIcon className="emojiPicker" />
+          </IconButton>
+          <IconButton
+            className="app__iconButton"
+            type="submit"
+            variant="contained"
+            color="primary"
+            disabled={!input}
+            onClick={sendMessage}
+          >
+            <SendIcon className="sendIcon" />
+          </IconButton>
+        </FormControl>
+      </form>
+
+      <div className="messages">
+        <FlipMove>
+          {messages.map(({ id, message }) => (
+            <Messages key={id} username={username} message={message} />
+          ))}
+        </FlipMove>
+      </div>
+      <div className="emojiContainer" id="mydiv">
         <Picker
-          className="emojiContainer"
           onEmojiClick={onEmojiClick}
           disableAutoFocus={true}
           groupNames={{ smileys_people: "PEOPLE" }}
         />
-      </FormControl>
-
-      <FlipMove>
-        {messages.map(({id,message}) => (
-          <Messages key={id} username={username} message={message} />
-          // <p>{message}</p>
-        ))}
-      </FlipMove>
+      </div>
     </div>
   );
 }
--- src/Messages.js
+++ src/Messages.js
@@ -1,28 +1,40 @@
 import React,{forwardRef} from "react";
 import { Card, CardContent,Typography} from '@material-ui/core';
 import  './messages.css';
+// import ScrollToBottom from 'react-scroll-to-bottom';
+var d = new Date(); 
+var day = d.getDate; 
+var month = d.getMonth; 
+var year = d.getFullYear; 
+
 
 const Messages = forwardRef(({username, message},ref)=> {
-    console.log(username)
-    console.log(message)
+    // console.log(username)
+    // console.log(message)
     const isUser = username === message.username;
     // const isUser= true;
+  //   const messageHeader = ()=>{
+  //     if(!isUser){
+  //       `${message.username} to everyone:`
+  //     }
+  //     else{`You to everyone:`}
+  //   }
   return (
-    <div ref={ref}>
-      <Card className={isUser? "message-user":"message-guest"} >
+    <div ref={ref} >
+      <p className={isUser? "message-user-name":"message-guest-name"}>{isUser?`You to everyone:`:`${message.username} to everyone:`}</p>
+      <Card className={isUser? "message-user":"message-guest"}  >
+        
         <CardContent>
-          <Typography className="user__name"  variant="h5" component="h4"> 
-          {message.username}
-          </Typography>
-          <hr></hr>
+         
           <Typography className="user__message" color="grey" variant="h5" component="h2"> 
           {message.message}
           </Typography>
-          {/* <Typography color="grey" variant="h5" component="h2"> 
-          {message.username}:{message.message}
-          </Typography> */}
+  {/* <p className={isUser? "message-user-dateTime":"message-guest-dateTime"}>{day}-{month}-{year}</p> */}
+          
         </CardContent>
       </Card>
+      
+
     </div>
   );
 })
